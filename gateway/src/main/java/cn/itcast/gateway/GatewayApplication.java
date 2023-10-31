0388214b26630cea001b19488eee4491c8253179
chen011209 <cyc7397221>
2023-10-31 17:03:51 +0800

    配置全局过滤器，可以进行业务的处理，如放行等

--- gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * 启动nacos 点击startup.cmd  或者命令行startup.cmd -m standalone
+ */
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
